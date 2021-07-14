c17c035b38785ff5333f262b5e15246d661e5333
Thangnt <thangnt336@gmail.com>
2021-07-14 19:05:37 -0400

    added activate handler to service worker js

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,7 +15,21 @@ const RUNTIME = 'runtime';
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
-            .open(PRECACHE).then((cache) => cache.addAll(FILES_TO_CACHE)).then(self.skipWaiting())
-    );
+            .open(PRECACHE).then((cache) => cache.addAll(FILES_TO_CACHE)).then(self.skipWaiting()));
 });
+
+// Activate to clear old cache
+self.addEventListener('activate', (event) => {
+    const current_cache = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then((cacheName) => {
+            return cacheName.filter((cache_name) => !current_cache.includes(cache_name));
+        }).then((cacheDelete) => {
+            return Promise.all(
+                cacheDelete.map((cache_delete) => {
+                    return caches.delete(cache_delete);
+                }));
+        }).then(() => self.clients.claim()));
+});
+
 
