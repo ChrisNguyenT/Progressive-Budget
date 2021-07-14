7d8080e912b5e4ab1146ecf458e9751d32e18c57
Thangnt <thangnt336@gmail.com>
2021-07-14 18:53:26 -0400

    added install script to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,14 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
 ];
 
+const PRECACHE = 'precache-test-v1';
+const RUNTIME = 'runtime';
+
+// Install
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE).then((cache) => cache.addAll(FILES_TO_CACHE)).then(self.skipWaiting())
+    );
+});
+
